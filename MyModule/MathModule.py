bc439a3dbc344d6c48eb1358ab7c854d7a60adaa
GodNona <datanhpham181208@gmail.com>
2024-12-04 11:27:01 +0700

    Test sqrt

--- MyModule/MathModule.py
+++ MyModule/MathModule.py
@@ -1,6 +1,26 @@
+# mày code vào branch_write
+# code xong chạy ngon thì pull request đấy để t còn xem đc code m
+
+def my_abs(value):
+    return value if value >= 0 else -value
+
+def my_sqrt(x):
+    ok = 0.000000000000001
+    kq = 1
+    while my_abs(kq * kq - x) / x >= ok:
+        kq = (x / kq + kq) / 2
+    return kq
+
+def hypotenuse(a, b):
+    c=a*a+b*b
+    return  my_sqrt(c)
+
 
 def main():
-    pass
+    n = 100
+    while n != 0:
+        print(my_sqrt(n))
+        n -= 1
 
 if __name__ == "__main__":
     main()
